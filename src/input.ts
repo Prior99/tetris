92454e25cb1311883576686232a9f869625398b1
Frederick Gnodtke <frederick@gnodtke.net>
2019-02-19 16:14:15 +0100

    Make input work

--- src/input.ts
+++ src/input.ts
@@ -1,8 +1,48 @@
-import { component } from "tsdi";
+import { component, initialize } from "tsdi";
+import { bind } from "lodash-decorators";
 
 @component
 export class Input {
     public moveLeft = false;
     public moveRight = false;
     public rotate = false;
+
+    @initialize protected initialize() {
+        window.addEventListener("keyup", this.handleKeyUp);
+        window.addEventListener("keydown", this.handleKeyDown);
+    }
+
+    @bind private handleKeyUp(evt: KeyboardEvent) {
+        switch (evt.key) {
+            case "ArrowUp":
+            case "w":
+                this.rotate = false;
+                break;
+            case "ArrowLeft":
+            case "a":
+                this.moveLeft = false;
+                break;
+            case "ArrowRight":
+            case "d":
+                this.moveRight = false;
+                break;
+        }
+    }
+
+    @bind private handleKeyDown(evt: KeyboardEvent) {
+        switch (evt.key) {
+            case "ArrowUp":
+            case "w":
+                this.rotate = true;
+                break;
+            case "ArrowLeft":
+            case "a":
+                this.moveLeft = true;
+                break;
+            case "ArrowRight":
+            case "d":
+                this.moveRight = true;
+                break;
+        }
+    }
 }
