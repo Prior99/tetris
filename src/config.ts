8f021b529378af296472e2804ba78e856ceeedbd
Frederick Gnodtke <frederick@gnodtke.net>
2019-03-03 17:42:14 +0100

    Make initial input delay shorter

--- src/config.ts
+++ src/config.ts
@@ -7,7 +7,7 @@ export class Config {
     public visibleSize = vec2(10, 20);
     public tickSpeed = 1 / 60;
     public networkSpeed = 10 / 60;
-    public initialInputTimeout = 15 / 60;
+    public initialInputTimeout = 10 / 60;
     public inputRepeatTimeout = 1.5 / 60;
     public inputRotateRepeatTimeout = 10 / 60;
     public tetriminoPixelSize = 32;
