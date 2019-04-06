effa21c154ea62dd0f1026d5cd2f66d925e82b5a
Frederick Gnodtke <frederick@gnodtke.net>
2019-04-06 21:55:25 +0200

    Remove mobx-react-devtools

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import DevTools from "mobx-react-devtools";
 import { TSDI } from "tsdi";
 import { FactoryAudioContext } from "factories";
 import { App } from "components";
@@ -14,13 +13,7 @@ async function main() {
     await tsdi.get(FactoryAudioContext).initialize();
 
     // Start React.
-    ReactDOM.render(
-        <>
-            <DevTools />
-            <App />
-        </>,
-        document.getElementById("root"),
-    );
+    ReactDOM.render(<App />, document.getElementById("root"));
 }
 
 main();
