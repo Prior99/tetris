8e39513163153424ce319033652d3d3280d0b338
Frederick Gnodtke <frederick@gnodtke.net>
2019-04-12 09:16:33 +0200

    Add initial unit tests for host

--- src/__mocks__/peerjs.ts
+++ src/__mocks__/peerjs.ts
@@ -1,14 +1,12 @@
+import * as PeerJS from "peerjs";
 import { DataConnection } from "peerjs";
 
-export class MockPeerJS {
+export class MockPeerJS implements PeerJS {
+    public prototype: RTCIceServer;
     public connectionListeners: ((connection: DataConnection) => void)[] = [];
-
     public id = "MOCKID00000";
-
     public connections = [];
-
     public disconnected = true;
-
     public destroyed = false;
 
     public connect = jest.fn();
@@ -20,7 +18,7 @@ export class MockPeerJS {
             case "open": callback(); return;
             case "connection": this.connectionListeners.push(callback);
         }
-    });
+    }) as any;
 
     public off = jest.fn();
 
@@ -36,6 +34,36 @@ export class MockPeerJS {
 
     public emulateConnection(connection: DataConnection) {
         this.connectionListeners.forEach(listener => listener(connection));
+    }
+}
+
+export class MockDataConnection implements DataConnection {
+    public dataListeners: ((data: any) => void)[] = [];
+    public dataChannel: RTCDataChannel;
+    public label: string;
+    public metadata: any;
+    public open: boolean;
+    public peerConnection: any;
+    public peer: string;
+    public reliable: boolean;
+    public serialization: string;
+    public type: string;
+    public buffSize: number;
+
+    public send = jest.fn();
+
+    public on = jest.fn((event: string, callback: () => void) => {
+        switch (event) {
+            case "data": this.dataListeners.push(callback);
+        }
+    }) as any;
+
+    public off = jest.fn();
+
+    public close = jest.fn();
+
+    public emulateData(data: any) {
+        this.dataListeners.forEach(listener => listener(data));
     }
 }
 
--- src/networking/__tests__/test-host.ts
+++ src/networking/__tests__/test-host.ts
@@ -2,7 +2,9 @@ import { Host } from "../host";
 import { RemoteUsers } from "../remote-users";
 import { NetworkGame } from "../network-game";
 import { Chat } from "../chat";
-import { MockPeerJS } from "../../__mocks__/peerjs";
+import { MockPeerJS, MockDataConnection } from "../../__mocks__/peerjs";
+import { MessageType } from "../messages";
+import { RemoteUser } from "../../types";
 
 describe("Host", () => {
     let host: Host;
@@ -11,11 +13,16 @@ describe("Host", () => {
     let networkGame: NetworkGame;
     let name: string;
     let mockPeerJS: MockPeerJS;
+    const ownUser = {
+        id: "MOCKID00000",
+        name: "Some random name",
+    };
 
     beforeEach(() => {
         chat = new Chat();
         remoteUsers = new RemoteUsers();
         networkGame = new NetworkGame(remoteUsers);
+        networkGame.parameters.seed = "some seed";
         name = "Some name";
         host = new Host(remoteUsers, chat, networkGame, name);
         mockPeerJS = (host as any).peer;
@@ -36,13 +43,60 @@ describe("Host", () => {
 
         it("stores the id", () => expect(host.id).toBe("MOCKID00000"));
 
-        it("adds the own user", () => expect(spySetOwnUser).toHaveBeenCalledWith({
-            id: "MOCKID00000",
-            name: "Some random name",
-        }));
+        it("adds the own user", () => expect(spySetOwnUser).toHaveBeenCalledWith(ownUser));
 
         it("listens for new connection", () => {
             expect(mockPeerJS.on).toHaveBeenCalledWith("connection", expect.any(Function));
+        });
+
+        describe("with two client connected", () => {
+            let connections: MockDataConnection[] = [];
+            let otherUsers: RemoteUser[] = [];
+
+            beforeEach(() => {
+                connections = [new MockDataConnection(), new MockDataConnection()];
+                otherUsers = connections.map((_, index) => ({
+                    name: `Another user #${index}`,
+                    id: `ANOTHERID0000${index}`,
+                }))
+                connections.forEach(connection => mockPeerJS.emulateConnection(connection));
+            });
+
+            it("subscribed to incoming data", () => connections.forEach(connection => {
+                expect(connection.on).toHaveBeenCalledWith("data", expect.any(Function));
+            }));
+
+            describe("with both clients having sent `HELLO`", () => {
+                beforeEach(() => connections.forEach((connection, index) => {
+                    connection.emulateData({
+                        message: MessageType.HELLO,
+                        user: otherUsers[index],
+                    });
+                }));
+
+                it("sent `WELCOME` with only the own user to the first connection", () => {
+                    expect(connections[0].send).toHaveBeenNthCalledWith(1, {
+                        message: MessageType.WELCOME,
+                        parameters: networkGame.parameters,
+                        users: [otherUsers[0], ownUser],
+                    });
+                });
+
+                it("sent `WELCOME` with two users to the second connection", () => {
+                    expect(connections[1].send).toHaveBeenNthCalledWith(1, {
+                        message: MessageType.WELCOME,
+                        parameters: networkGame.parameters,
+                        users: [otherUsers[0], otherUsers[1], ownUser],
+                    });
+                });
+
+                it("sent `USER_CONNECTED` message to first connection", () => {
+                    expect(connections[0].send).toHaveBeenNthCalledWith(2, {
+                        message: MessageType.USER_CONNECTED,
+                        user: otherUsers[1],
+                    });
+                });
+            });
         });
     });
 });
