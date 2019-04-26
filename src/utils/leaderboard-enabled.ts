3fd39ef3d816d3f65e20c2bd7e45c879f976af50
Frederick Gnodtke <frederick@gnodtke.net>
2019-04-26 08:18:03 +0200

    Fix linting error

--- src/utils/leaderboard-enabled.ts
+++ src/utils/leaderboard-enabled.ts
@@ -5,7 +5,7 @@ const allowedConditions = [WinningConditionType.HIGHEST_SCORE_ONE_GAME, WinningC
 export function leaderboardEnabled(parameters: GameParameters): boolean {
     if (!allowedConditions.includes(parameters.winningCondition.condition)) { return false; }
     if (parameters.winningCondition.condition === WinningConditionType.BATTLE_ROYALE) {
-        if (parameters.winningCondition.lives != 1) { return false; }
+        if (parameters.winningCondition.lives !== 1) { return false; }
     }
     if (parameters.initialLevel !== 0) { return false; }
     if (parameters.levelUpDisabled) { return false; }
